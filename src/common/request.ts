2b65f3da6257bd7cbb510fb502bb82b4ac6e6c2f
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2022-07-12 00:46:29 +0300

    Add http request (fetch) helpers and use it with dummies

---
+++ src/common/request.ts
@@ -0,0 +1,33 @@
+const apiBase = 'https://62cb4dbb3e924a012865c364.mockapi.io/';
+
+const defaultHeaders: HeadersInit = {
+  'Content-Type': 'application/json;charset=UTF-8',
+};
+
+const fetchRequest = <TResponse>(url: string, config: RequestInit = {}): Promise<TResponse> =>
+  fetch(`${apiBase}${url}`, config)
+    .then((response) => response.json())
+    .then((data) => data as TResponse);
+
+const request = {
+  get: <TResponse>(url: string) =>
+    fetchRequest<TResponse>(url, { method: 'GET', headers: defaultHeaders }),
+
+  delete: <TResponse>(url: string) =>
+    fetchRequest<TResponse>(url, { method: 'DELETE', headers: defaultHeaders }),
+
+  post: <TBody extends BodyInit, TResponse>(url: string, body: TBody) =>
+    fetchRequest<TResponse>(url, { method: 'POST', body, headers: defaultHeaders }),
+
+  put: <TBody extends BodyInit, TResponse>(url: string, body: TBody) =>
+    fetchRequest<TResponse>(url, { method: 'PUT', body, headers: defaultHeaders }),
+};
+
+export enum ERequestStatus {
+  IDLE = 'IDLE',
+  LOADING = 'LOADING',
+  SUCCEEDED = 'SUCCEEDED',
+  FAILED = 'FAILED',
+}
+
+export default request;
--- src/routes/About/index.tsx
+++ src/routes/About/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserList from '../../components/partials/About/Users';
 import Quote from '../../components/shareds/Quote';
 
 const About = () => (
   <div className="About">
     About works!
+    <UserList />
     <Quote />
   </div>
 );
